81581a650d47b2c89ccc88eced2afa273d9adbe7
Irina <zaletk.irina@gmail.com>
2019-12-17 17:49:00 +0300

    feat : add solution from 25 to 27

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -159,8 +159,9 @@ function parseNumberFromString(value) {
  *   3,3,3   => 5.196152422706632
  *   1,2,3   => 3.741657386773941
  */
-function getParallelipidedDiagonal(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getParallelipidedDiagonal(a, b, c) {
+  const d = a ** 2 + b ** 2 + c ** 2;
+  return Math.sqrt(d);
 }
 
 /**
@@ -180,9 +181,11 @@ function getParallelipidedDiagonal(/* a, b, c */) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  const st = 10 ** pow;
+  return Math.round(num / st) * st;
 }
+
 
 /**
  * Returns true is the number is prime; otherwise false.
@@ -201,8 +204,14 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  // eslint-disable-next-line no-plusplus
+  for (let i = 2; i <= n / 2; i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /**
